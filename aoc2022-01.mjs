3e9c6a380c97a3cd188871deb57139a9d3c07673
TomoFromEarth <mikedonothackmethanks@gmail.com>
2022-12-01 23:20:03 -0500

    final solution for aoc2022-day01

---
+++ aoc2022-01.mjs
@@ -0,0 +1,20 @@
+import { readFileSync } from "fs";
+
+// convert .txt file to array of strings
+const getElves = readFileSync("./input.txt", "utf-8").split(/\r?\n\n/);
+
+// convert string array into array of integer groups and reduce to single integer
+const getTotalCalories = getElves.map((elf) => {
+  let calories = elf.split(/\n/).map((elf) => parseInt(elf, 10));
+
+  let totalCalories = calories.reduce((acc, cur) => acc + cur, 0);
+  return totalCalories;
+});
+
+// get biggest bag of calories
+console.log(getTotalCalories.sort((a, b) => b - a)[0]);
+
+// get top three bags o calories average
+const topThreeElves = getTotalCalories.sort((a, b) => b - a);
+topThreeElves.splice(3);
+console.log(topThreeElves.reduce((a, b) => a + b, 0));
